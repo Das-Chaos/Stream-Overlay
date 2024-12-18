894a8a82eb4de1988566e26d20f8767bc4912759
Das-Chaos <florian.theunissen@icloud.com>
2024-12-18 06:25:38 +0100

    Temporarily disable rate limiting

--- middleware/rateLimit.ts
+++ middleware/rateLimit.ts
@@ -1,17 +1,5 @@
-import rateLimit from 'express-rate-limit'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-})
-
 export default function rateLimitMiddleware(req: NextApiRequest, res: NextApiResponse, next: () => void) {
-  return new Promise((resolve, reject) => {
-    limiter(req, res, (result: Error | undefined) => {
-      if (result instanceof Error) return reject(result)
-      return resolve(result)
-    })
-  })
+  return Promise.resolve()
 }
-
