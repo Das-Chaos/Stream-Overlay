295ae684ecc4738c72dea4cde6d7d7780392222c
Das-Chaos <florian.theunissen@icloud.com>
2024-12-18 06:55:30 +0100

    Fix: Update authorize callback to return correct user fields

--- app/api/auth/auth.config.ts
+++ app/api/auth/auth.config.ts
@@ -36,7 +36,8 @@ export const authOptions = {
         return {
           id: user.id,
           email: user.email,
-          name: user.name
+          username: user.username,
+          role: user.role
         }
       }
     })
